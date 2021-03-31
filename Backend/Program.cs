8961d1409a265338189558c4fc99f366a9fdf711
ethereum <boris.saric@gmail.com>
2021-03-31 19:37:27 +0200

    implemented fluent migrator

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,13 +7,19 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Backend.Data;
+using Backend.Services;
+
 namespace Backend
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+		.Build()
+		.MigrateDatabase<ApplicationDbContext>()
+		.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Backend/Services/Extensions.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using Backend.Data;
+
+namespace Backend.Services
+{ 
+	public static class Extensions
+	{
+
+		public static IHost MigrateDatabase<DbContext>(this IHost webHost)
+		{
+    			var serviceScopeFactory =  webHost.Services;
+
+    			using(var scope = serviceScopeFactory.CreateScope())
+    			{
+        			var services = scope.ServiceProvider;
+        			try
+        			{
+					SeedData(services.GetRequiredService<ApplicationDbContext>());
+            				 
+       				 }
+        			catch (Exception ex)
+        			{
+            				var logger = services.GetRequiredService<ILogger<Program>>();
+            				logger.LogError(ex, "An error occurred while migrating the database.");
+        			}
+    			}
+    			return webHost;
+		}
+	        public static void SeedData(ApplicationDbContext context)
+		{
+			System.Console.WriteLine("Applying migrations...");
+			context.Database.Migrate();
+		}
+	}
+}
